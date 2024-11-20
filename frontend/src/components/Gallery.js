137309c103ea8dc7545b41a9c32d06f0bf7f292f
Islam <islameoa@gmail.com>
2024-11-20 19:40:34 +0100

    base is-studio project

--- frontend/src/components/Gallery.js
+++ frontend/src/components/Gallery.js
@@ -1,22 +1,40 @@
 import React from 'react';
-import './Gallery.css';
-
+import '../styles/Gallery.css';
+import image1 from '../images/image1.jpg';
+import image2 from '../images/image1.jpg';
+import image3 from '../images/image1.jpg';
+import image4 from '../images/image1.jpg';
+import image5 from '../images/image1.jpg';
+import image6 from '../images/image1.jpg';
+import image7 from '../images/image1.jpg';
+import image8 from '../images/image1.jpg';
+import image9 from '../images/image1.jpg';
+import image10 from '../images/image1.jpg';
+import homeLogo from '../images/logo_biggest.png';
 const images = [
-  'image1.jpg',
-  'image2.jpg',
-  'image3.jpg',
-  'image4.jpg',
-  'image5.jpg',
+  image1,
+  image2,
+  image3,
+  image4,
+  image5,
+  image6,
+  image7,
+  image8,
+  image9,
+  image10,
 ];
 
 const Gallery = () => {
   return (
-    <div className="gallery">
-      {images.map((image, index) => (
-        <div key={index} className="gallery-item">
-          <img src={image} alt={`Artwork ${index + 1}`} />
-        </div>
-      ))}
+    <div className="gallery-container">
+      <img src={homeLogo} alt="is-studio logo" className='logo'/>
+      <div className="gallery">
+        {images.map((image, index) => (
+          <div key={index} className="gallery-item">
+            <img src={image} alt={`Artwork ${index + 1}`} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,16 +1,49 @@
-import React from 'react';
-import './Navbar.css';
-
+import React, { useState, useEffect } from 'react';
+import '../styles/Navbar.css';
+import logoSmall from '../images/lilogo_square.jpeg';
+import logo from '../images/header_logo.png';
+import menuIcon from '../images/menu_icon.png';
 const Navbar = () => {
+  const [scrolled, setScrolled] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const handleScroll = () => setScrolled(window.scrollY > 50);
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  const handleMouseMove = (e) => {
+    setMousePosition({ x: e.clientX, y: e.clientY });
+  };
+
   return (
-    <nav className="navbar">
-      <h1>Collection</h1>
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
-      </ul>
-    </nav>
+    <div className="navbar-container" onMouseMove={handleMouseMove}>
+      <nav className={`navbar ${scrolled ? 'navbar-scrolled' : ''}`}>
+        <img src={scrolled ? logo : logoSmall} alt="Logo" className="navbar-logo" />
+        <ul className={`nav-links-straight ${!scrolled ? 'show' : ''}`}>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+        <div className={`menu-icon ${scrolled ? 'show' : ''}`} onClick={() => setMenuOpen(!menuOpen)}>
+          <img src={menuIcon} alt="Menu Icon" />
+        </div>
+        <ul className={`nav-links ${menuOpen ? 'open' : ''}`}>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+        <div
+          className="white-circle"
+          style={{
+            left: `${mousePosition.x}px`,
+            top: `${mousePosition.y}px`,
+          }}
+        ></div>
+      </nav>
+    </div>
   );
 };
 
